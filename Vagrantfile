1008637e5e513bc8590ec36e7f5965cb3b036eb2
jona <yablee@gmail.com>
2017-07-29 20:00:46 +0200

    vagrantfile and provision

--- Vagrantfile
+++ Vagrantfile
@@ -5,13 +5,13 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 1
+  end
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "private_network", ip: "192.168.33.10"
   config.vm.network "forwarded_port", guest: 80, host: 8080
-
-  config.vm.provider "virtualbox" do |vb|
-    vb.customize ["modifyvm", :id, "--memory", "1024"]
-  end
 
   config.vm.provision "shell", path: "provision.sh"
 
--- provision.sh
+++ provision.sh
@@ -11,23 +11,33 @@ sudo apt-get -y install git-core nginx
 
 # Mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get -y install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl enable mongod.service
 
+export LC_ALL=C
 
 ## The following to set up the app
-git clone https://X/Y
-cd Y
-sudo pip install python-virtualenv
+git clone https://github.com/jonathanronen/smapp-anes-facebook-survey-addon.git
+cd smapp-anes-facebook-survey-addon
 sudo apt-get -y install python-pip
+sudo pip install -U pip
+sudo apt-get install -y python-virtualenv
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 
 echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
 echo ". /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc 
 
 mkdir ~/.virtualenvs
 mkvirtualenv smapp-anes
 
 pip install -r requirements.txt
+pip install gunicorn
+
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/config.backup
+sudo cp nginx.default.config /etc/nginx/sites-available/default
+sudo systemctl restart nginx.service
